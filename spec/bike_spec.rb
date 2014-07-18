403e0a1f80167ce1323e0101672e85413711e1a1
Lisa A-R <lisaar@gmail.com>
2014-07-18 16:15:59 +0100

    bike.rb

---
+++ spec/bike_spec.rb
@@ -0,0 +1,19 @@
+require 'bike'
+
+describe Bike do
+	let(:bike) {Bike.new}
+	it 'should not be broken after we create it' do
+		expect(bike.broken?).to be false
+	end
+
+	it 'should be able to break' do
+		bike.break!
+		expect(bike.broken?).to be true
+	end
+
+	it 'should be able to get fixed' do
+		bike.break!
+		bike.fix!
+		expect(bike.broken?).to be false
+	end
+end
