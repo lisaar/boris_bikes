d59888840efcca33c4f077c0d03cdb18e3c09f1f
Lisa A-R <lisaar@gmail.com>
2014-07-18 16:51:14 +0100

    bike_container.rb

--- lib/station.rb
+++ lib/station.rb
@@ -1,28 +1,11 @@
+require_relative 'bike_container'
 class Station
-	DEFAULT_CAPACITY = 10
+	
+	include BikeContainer
+
 	def initialize(options ={})
-		@bikes = []
-		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-	end
-
-	def bike_count
-		@bikes.count
-	end
-
-	def dock(bike)
-		raise "Station is full" if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		@bikes.delete(bike)
-	end
-
-	def full?
-		bike_count == @capacity
-	end
-
-	def available_bikes
-		@bikes.reject {|bike| bike.broken?}
-	end
+		self.capacity = options.fetch(:capacity, capacity)
+	end	
+	
 end
+
